b48d9fa9bfaea7f5f6215fab5206db079e8c6c14
Christian <ps5christian@hotmail.com>
2024-01-27 23:45:54 +0100

    Added props to components

--- React-Basics/src/App.jsx
+++ React-Basics/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
+import Students from "./Students.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
@@ -9,6 +10,15 @@ function App() {
       <Header></Header>
       <Food></Food>
       <Card></Card>
+      <Students
+        name="Spongebob Squarepants"
+        age={30}
+        isStudent={true}
+      ></Students>
+      <Students name="Patrick Starfish" age={42} isStudent={false}></Students>
+      <Students name="Squidward" age={50} isStudent={false}></Students>
+      <Students name="Sandy" age={27} isStudent={true}></Students>
+      <Students name="Joe"></Students>
       <Footer></Footer>
     </>
   );
